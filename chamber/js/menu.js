eb4fc9238f3353df02115a75f3421b6d0bfd4481
Your Name <you@example.com>
2022-02-15 10:33:12 -0500

    X icon

--- chamber/js/menu.js
+++ chamber/js/menu.js
@@ -1,5 +1,6 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("HAMBURGERbTN").classList.toggle("open");
 }
 
 const x = document.getElementById('hamburgerBtn');
