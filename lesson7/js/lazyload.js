60e9f8e0172f18a3a4f2dfe2dc8809cc96e8e903
Your Name <you@example.com>
2022-02-18 10:53:18 -0500

    delete optionimg

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -6,10 +6,6 @@ const loadImages = (image) => {
     }
 }
 
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px, 0px, 50px, 0px"
-};
 
 if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
@@ -19,9 +15,9 @@ if('IntersectionObserver' in window) {
           observer.unobserve(item.target);
         }
       });
-    }, imgOptions);
+    }, );
 
-    
+
     imagesToLoad.forEach((img) => {
       observer.observe(img);
     });
