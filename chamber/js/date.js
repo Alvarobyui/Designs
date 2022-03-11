98e4c2a9d645e8278236391255cc25ca28724759
Your Name <you@example.com>
2022-03-11 12:35:08 -0500

    index and css files validated. :)

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -21,3 +21,15 @@ const year = now.getFullYear();
 document.querySelector(
 	"#onlyYear"
 ).textContent = `${year}`;
+
+
+
+//chamber meeting disaplays only on mondays and tuesdays
+document.getElementById("meetingBanner").style.display = "none";
+const day = now.getDay();
+if (day == 1) {
+	document.getElementById("meetingBanner").style.display = "block";
+};
+if (day == 2) {
+	document.getElementById("meetingBanner").style.display = "block";
+	};
