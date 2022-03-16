cd087e89e04dde0c433e5d5d58905a81ee285a98
Your Name <you@example.com>
2022-03-15 19:43:45 -0500

    large.css fixed up and some other details

--- chamber/js/lazyLoad.js
+++ chamber/js/lazyLoad.js
@@ -1,19 +1,32 @@
 const images = document.querySelectorAll("[data-src]");
 
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
 
+    img.src = src;
+}
 
-
-const imgOptions = {};
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 100px 0px"
+};
 
 const imgObserver = new IntersectionObserver((entries,
      imgObserver) => {
         entries.forEach(entry => {
-            if (!entry.isIntersection) {
+            if (!entry.isIntersecting) {
                 return;
             } else {
-                preLoadImage(enry.target);
+                preloadImage(entry.target);
                 imgObserver.unobserve(entry.target);
             }
         })
 
 }, imgOptions)
+
+images.forEach(image => {
+    imgObserver.observe(image);
+})
