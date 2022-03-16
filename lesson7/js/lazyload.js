d3d4eec1dc9b4ab0fda92bcaa76cf2b32490ce10
Your Name <you@example.com>
2022-03-16 09:57:34 -0500

    lazyload for images

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,31 +1,32 @@
-let imagesToLoad = document.querySelectorAll("img[data-src]");
-const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () => {
-        image.removeAttribute("data-src");
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
     }
+
+    img.src = src;
 }
 
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 100px 0px"
+};
 
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    }, );
+const imgObserver = new IntersectionObserver((entries,
+     imgObserver) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                imgObserver.unobserve(entry.target);
+            }
+        })
 
+}, imgOptions)
 
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } 
-else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
-
-
+images.forEach(image => {
+    imgObserver.observe(image);
+})
