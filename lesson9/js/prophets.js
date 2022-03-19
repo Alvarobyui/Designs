ec06d16d74ba6107e78e41d7f5ac58c960d5a224
Your Name <you@example.com>
2022-03-19 12:57:19 -0500

    prophets activity fixed up

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,33 +1,35 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
+
 
 fetch(requestURL) 
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    //console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
+    const prophets = jsonObject['prophets'];  
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
     prophets.forEach(displayProphets);
   }); 
 
-  function displayProphets(prophet) {
-    // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-  
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
-  
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    ____.setAttribute('src', prophet.imageurl);
-    ____.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
-    ____.setAttribute('loading', 'lazy');
-  
-    // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
-    card.appendChild(portrait);
-  
-    // Add/append the existing HTML div with the cards class with the section(card)
-    cards.appendChild(card);
-  }
+function displayProphets(prophet) {
+  // Create elements to add to the document
+  let card = document.createElement('section');
+  let h2 = document.createElement('h2');
+  let portrait = document.createElement('img');
+
+  // Change the textContent property of the h2 element to contain the prophet's full name
+  h2.textContent = `${prophet.name} ${prophet.lastname}`;
+
+  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+  portrait.setAttribute('src', prophet.imageurl);
+  portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-day President`);
+  portrait.setAttribute('loading', 'lazy');
+//`string text ${expression} string text`
+  // Add/append the section(card) with the h2 element
+  card.appendChild(h2);
+  card.appendChild(portrait);
+
+  // Add/append the existing HTML div with the cards class with the section(card)
+  cards.appendChild(card);
+}
