230ab30ce171a8ba64865ae0f9dfc06c0e9e899c
Your Name <you@example.com>
2022-03-19 14:00:07 -0500

    prophets activity finished

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -16,10 +16,14 @@ function displayProphets(prophet) {
   // Create elements to add to the document
   let card = document.createElement('section');
   let h2 = document.createElement('h2');
+  let p = document.createElement('p');
+  let p2 = document.createElement('p');
   let portrait = document.createElement('img');
 
   // Change the textContent property of the h2 element to contain the prophet's full name
   h2.textContent = `${prophet.name} ${prophet.lastname}`;
+  p.textContent = `Date of Birth: ${prophet.birthdate}`;
+  p2.textContent = `Place of Birth: ${prophet.birthplace}`;
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
   portrait.setAttribute('src', prophet.imageurl);
@@ -28,6 +32,8 @@ function displayProphets(prophet) {
 //`string text ${expression} string text`
   // Add/append the section(card) with the h2 element
   card.appendChild(h2);
+  card.appendChild(p);
+  card.appendChild(p2);
   card.appendChild(portrait);
 
   // Add/append the existing HTML div with the cards class with the section(card)
